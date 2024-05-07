b360993ec77e09d7727057bf7780857c53bc9182
RalonsoS <ralonso@stemdo.io>
2024-05-07 13:55:01 +0200

    ejercicio 5

---
+++ soluciones/ejercicio5/project/modules/ejercicio4/outputs.tf
@@ -0,0 +1,4 @@
+output "resource_group" {
+  description = "Info de la del grupo de recursos"
+  value = azurerm_virtual_network.mi_vnet.id
+}
---
+++ soluciones/ejercicio5/steps.md
@@ -0,0 +1,2 @@
+![alt text](./assets/img/plan.png)
+![alt text](./assets/img/apply.png)
