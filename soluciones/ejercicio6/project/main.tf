f40f368f8522250cd804ad0e56dcb9034f48c294
RalonsoS <ralonso@stemdo.io>
2024-05-08 09:23:20 +0200

    Ejercicio 6

---
+++ soluciones/ejercicio6/project/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  valid_values = ["DEV", "PRO", "TES", "PRE"]
+}
+
+module "ejercicio4" {
+  source = "github.com/stemdo-labs/terraform-exercises-RalonsoS-1/soluciones/ejercicio5/project/modules/ejercicio4"
+  existent_resource_group_name = var.existent_resource_group_name
+  owner_tag = var.owner_tag
+  vnet_name = var.vnet_name
+  vnet_address_space = var.vnet_address_space
+  vnet_tags = var.vnet_tags
+  environment_tag = var.environment_tag
+}
+
+
+
+
---
+++ soluciones/ejercicio6/steps.md
@@ -0,0 +1,3 @@
+![alt text](./assets/images/init.png)
+![alt text](./assets/images/plan.png)
+![alt text](./assets/images/apply.png)
