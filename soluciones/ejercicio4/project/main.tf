067ed1c49be68d635aa675131a87a616e7e2220f
RalonsoS <ralonso@stemdo.io>
2024-05-09 15:11:34 +0200

    Ejercicio 8

--- soluciones/ejercicio4/project/main.tf
+++ soluciones/ejercicio4/project/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+
+  backend "azurerm" {
+      resource_group_name  = "rg1ralonso-lab01"
+      storage_account_name = "sta1ralonso"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
